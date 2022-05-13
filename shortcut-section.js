08904b163166fe5d4e526cfad064a99e52c502fb
MilovanJevtovic <milovanjevtovicposlovni@gmail.com>
2022-05-13 17:08:43 +0200

    Almost finished the shortcut section
    Needs some debugging

--- shortcut-section.js
+++ shortcut-section.js
@@ -1,1 +1,19 @@
 const shortCut = document.getElementById("shortcut");
+var a = 0;
+
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.scrollY < 100) {
+        shortCut.style.display = "none";
+    } else if (window.scrollY > 100) {
+        shortCut.style.display = "block";
+    }
+}
+
+const closeButton = document.getElementById("iksic-button");
+
+closeButton.addEventListener("click", function() {
+    shortCut.style.display = "none";
+})
