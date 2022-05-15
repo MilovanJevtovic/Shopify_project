748ed01a10e8e7a1dc62c17d352cc6dfd731d70d
MilovanJevtovic <milovanjevtovicposlovni@gmail.com>
2022-05-15 14:27:56 +0200

    Finished the shortcut section
    Debugged the X button

--- shortcut-section.js
+++ shortcut-section.js
@@ -5,6 +5,7 @@ var a = 0;
 window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
+
     if (window.scrollY < 100) {
         shortCut.style.display = "none";
     } else if (window.scrollY > 100) {
@@ -15,5 +16,5 @@ function scrollFunction() {
 const closeButton = document.getElementById("iksic-button");
 
 closeButton.addEventListener("click", function() {
-    shortCut.style.display = "none";
+    shortCut.style.visibility = "hidden";
 })
